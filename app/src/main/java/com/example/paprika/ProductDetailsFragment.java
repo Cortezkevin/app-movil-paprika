7b5f47947c4fe3c3bdf2fd62bb28daed2c2f6839
Cortezkevin <A19105415@idat.edu.pe>
2021-12-26 19:28:35 -0500

    detalle de productos

--- app/src/main/java/com/example/paprika/ProductDetailsFragment.java
+++ app/src/main/java/com/example/paprika/ProductDetailsFragment.java
@@ -2,6 +2,7 @@ package com.example.paprika;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +18,8 @@ import com.example.paprika.API.RetrofitService;
 import com.example.paprika.API.Service.ProductService;
 import com.example.paprika.Model.Product;
 import com.example.paprika.Network.ImageRequester;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;;
@@ -38,17 +41,28 @@ public class ProductDetailsFragment extends Fragment {
     private TextView description;
     private TextView mark;
 
+    private TextInputEditText amount;
+    private MaterialButton increase;
+    private MaterialButton decrease;
 
-    /*private static String id_product;
-    private static String name_product;
-    private static Double price_product;*/
+
+    private TextView discount;
+    private TextView subtotal_price;
 
 
     @Override
     public void onCreate(@Nullable Bundle savedStateInstance) {
         super.onCreate(savedStateInstance);
-
     }
+    // contador de cantidad de productos
+    int amount_product = 1;
+    // descuento del producto por cantidad de unidades
+    double dsc = 0.0;
+    // cantidad de unidades del producto para hacer el descuento
+    int cant = 0;
+    // subtotal del pedido, precio por cantidad
+    Double price_unit_product = 0.0;
+    Double subtotal = 0.0;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -57,13 +71,7 @@ public class ProductDetailsFragment extends Fragment {
         getParentFragmentManager().setFragmentResultListener("params", this, new FragmentResultListener() {
             @Override
             public void onFragmentResult(@NonNull String key, @NonNull Bundle bundle) {
-                //id_product = bundle.getString("product_id");
-                /*name_product = bundle.getString("product_name");
-                price_product = bundle.getDouble("product_price");*/
-                //bundle.clear();
                 getProductById(bundle.getString("product_id"));
-                /*name.setText(name_product);
-                price.setText(price_product.toString());*/
             }
         });
 
@@ -76,11 +84,49 @@ public class ProductDetailsFragment extends Fragment {
         expiration_date = view.findViewById(R.id.text_product_expiration_date);
         description = view.findViewById(R.id.text_product_description);
 
+        amount = view.findViewById(R.id.product_amount);
+        increase = view.findViewById(R.id.button_increase);
+        decrease = view.findViewById(R.id.button_decrease);
 
+        discount = view.findViewById(R.id.discount_product);
+        subtotal_price = view.findViewById(R.id.subtotal_price_product);
+
+        amount.setText(amount_product + "");
+
+
+        increase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int max_amount = Integer.parseInt(stock.getText().toString());
+                if (amount_product < max_amount) {
+                    amount_product += 1;
+                    amount.setText(amount_product + "");
+
+                    subtotal = subtotal + price_unit_product ;
+                    subtotal_price.setText(subtotal + "");
+                }else{
+                    amount.setText(amount_product+"");
+                }
+            }
+        });
+        decrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (amount_product >= 2) {
+                    amount_product -= 1;
+                    amount.setText(amount_product + "");
+
+                    subtotal = subtotal - price_unit_product ;
+                    subtotal_price.setText(subtotal + "");
+                } else {
+                    amount.setText(amount_product + "");
+                }
+            }
+        });
         return view;
     }
 
-   public void getProductById(String id_product) {
+    public void getProductById(String id_product) {
         ProductService productService = RetrofitService.getRetrofit().create(ProductService.class);
         Call<Product> call = productService.getProductById(id_product);
         call.enqueue(new Callback<Product>() {
@@ -91,17 +137,20 @@ public class ProductDetailsFragment extends Fragment {
                     id.setText(obj.getId_product());
                     name.setText(obj.getName());
                     mark.setText(obj.getMark());
-                    price.setText(obj.getPrice().toString());
+                    price.setText("S/." + obj.getPrice().toString());
+                    subtotal_price.setText(obj.getPrice().toString());
+                    price_unit_product = obj.getPrice();
+                    subtotal = obj.getPrice();
+                    setDiscountCantProducts(obj.getPrice());
+                    discount.setText("Descuento de "+dsc*100+"% a partir de "+ cant +" unidades");
                     description.setText(obj.getDescription());
 
-                    //SimpleDateFormat d = new SimpleDateFormat("dd-MM-yy");
                     Date date = obj.getExpiration_date();
                     expiration_date.setText(DateFormat.getDateInstance().format(date));
                     stock.setText(obj.getStock().toString());
 
                     ImageRequester imageRequester = ImageRequester.getInstance();
                     imageRequester.setImageFromUrl(image, obj.getUrl_image());
-                    Log.e("RESPONSE BODY",""+response.body());
                 }
             }
 
@@ -110,5 +159,18 @@ public class ProductDetailsFragment extends Fragment {
                 Log.e("ERROR", "" + t.getMessage());
             }
         });
+    }
+
+    public void setDiscountCantProducts(Double priceUnit){
+        if (priceUnit > 1.0 && priceUnit < 8.0) {
+            cant = 10;
+            dsc = 0.02;
+        }else if(priceUnit > 8.0 && priceUnit < 16.0){
+            cant = 9;
+            dsc = 0.03;
+        }else if(priceUnit > 16.0 && priceUnit < 24.0) {
+            cant = 8;
+            dsc = 0.04;
+        }
     }
 }
